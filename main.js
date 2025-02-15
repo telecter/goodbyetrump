ee55c31bc8485bdf6a122a547af0ba5a81548225
telecter <alexcidon@icloud.com>
2025-02-15 17:05:00 -0500

    Add weeks, and hours

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-const countdown = document.getElementById("countdown");
-const endTime = new Date(2029, 0, 20, 12, 0, 0);
+const countdownDays = document.getElementById("countdownDays");
+const countdownWeeks = document.getElementById("countdownWeeks");
+const countdownHours = document.getElementById("countdownHours");
 
+const endTime = new Date(2029, 0, 20, 12, 0, 0);
 const now = new Date();
+
 const diffTime = Math.abs(endTime - now);
 const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-countdown.innerHTML = `${diffDays} days<br>`
+const diffWeeks = Math.ceil(diffDays / 7); 
+const diffHours = Math.round(diffTime / (1000 * 60 * 60));
+
+countdownDays.innerHTML = `${diffDays} days`;
+countdownWeeks.innerHTML = `${diffWeeks} weeks`;
+countdownHours.innerHTML = `${diffHours} hours`
