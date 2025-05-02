59c10f40828a7dfca1a73dccdae86e29714a9244
telecter <alexcidon9@gmail.com>
2025-05-02 18:07:09 -0400

    update overall website style

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const countdownDays = document.getElementById("countdownDays");
-const countdownWeeks = document.getElementById("countdownWeeks");
-const countdownHours = document.getElementById("countdownHours");
+const countdownDays = document.getElementById("countdown-days");
+const countdownWeeks = document.getElementById("countdown-weeks");
+const countdownHours = document.getElementById("countdown-hours");
 
 const endTime = new Date(2029, 0, 20, 12, 0, 0);
 const now = new Date();
 
 const diffTime = Math.abs(endTime - now);
 const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
-const diffWeeks = Math.ceil(diffDays / 7); 
+const diffWeeks = Math.ceil(diffDays / 7);
 const diffHours = Math.round(diffTime / (1000 * 60 * 60));
 
 countdownDays.innerHTML = `${diffDays} days`;
 countdownWeeks.innerHTML = `${diffWeeks} weeks`;
-countdownHours.innerHTML = `${diffHours} hours`
+countdownHours.innerHTML = `${diffHours} hours`;
