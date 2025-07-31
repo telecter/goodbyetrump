87d44d78b7ee1d124bba86af748adf00f912ba98
telecter <102524245+telecter@users.noreply.github.com>
2025-07-31 19:44:37 -0400

    Change countdown layout, add months counter

--- src/js/main.js
+++ src/js/main.js
@@ -6,9 +6,7 @@
  \____|\___/ \___/ \__,_|_.__/ \__, |\___|   |_||_|   \__,_|_| |_| |_| .__/(_)
                                |___/                                 |_|      
 
-Copyright (c) 2025 telecter - https://github.com/telecter/goodbyetrump
-Licensed MIT, see LICENSE file.
-
+https://github.com/telecter/goodbyetrump
 */
 
 import { CountUp } from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.9.0/countUp.min.js";
@@ -29,62 +27,71 @@ document.body.setAttribute(
   localStorage.getItem("color-scheme") || "dark"
 );
 
-const countdownDays = document.getElementById("countdown-days");
-const countdownWeeks = document.getElementById("countdown-weeks");
-const countdownHours = document.getElementById("countdown-hours");
+const daysText = document.getElementById("days");
+const monthsText = document.getElementById("months");
+const weeksText = document.getElementById("weeks");
+const hoursText = document.getElementById("hours");
 const progress = document.getElementById("countdown-progress");
 const progressLabel = document.getElementById("progress-label");
+const endModal = document.getElementById("end-modal");
 
 function getDiffUnits(diffTime) {
   const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+  const diffMonths = diffDays / 30.436768;
   const diffWeeks = Math.ceil(diffDays / 7);
   const diffHours = Math.round(diffTime / (1000 * 60 * 60));
-  return [diffWeeks, diffDays, diffHours];
+  return [diffMonths, diffWeeks, diffDays, diffHours];
 }
 
 function setCountdown(startTime, endTime) {
-  const now = new Date();
-  const [totalWeeks, totalDays, totalHours] = getDiffUnits(endTime - startTime);
+  let now = new Date();
+  const [totalMonths, totalWeeks, totalDays, totalHours] = getDiffUnits(
+    endTime - startTime
+  );
 
-  if (endTime - now > 0) {
-    const [diffWeeks, diffDays, diffHours] = getDiffUnits(endTime - now);
-    const decimalComplete = 1 - diffDays / 1461;
-    const percentComplete = (decimalComplete * 100).toFixed(1);
+  if (endTime - now < 0) {
+    now = endTime;
+    endModal.showModal();
+  }
 
-    new CountUp(countdownDays, diffDays, {
-      startVal: totalDays,
-      suffix: diffDays == 1 ? " day" : " days",
-      separator: "",
-    }).start();
+  const [diffMonths, diffWeeks, diffDays, diffHours] = getDiffUnits(
+    endTime - now
+  );
+  const decimalComplete = 1 - diffDays / 1461;
+  const percentComplete = (decimalComplete * 100).toFixed(1);
 
-    new CountUp(countdownWeeks, diffWeeks, {
-      startVal: totalWeeks,
-      suffix: diffWeeks == 1 ? " week" : " weeks",
-      separator: "",
-    }).start();
+  new CountUp(daysText, diffDays, {
+    startVal: totalDays,
+    separator: "",
+  }).start();
 
-    new CountUp(countdownHours, diffHours, {
-      startVal: totalHours,
-      suffix: diffHours == 1 ? " hour" : " hours",
-      separator: "",
-    }).start();
+  new CountUp(monthsText, diffMonths, {
+    startVal: totalMonths,
+  }).start();
 
-    new CountUp(progressLabel, percentComplete, {
-      suffix: "% complete",
-      decimalPlaces: 1,
-    }).start();
+  new CountUp(weeksText, diffWeeks, {
+    startVal: totalWeeks,
+    separator: "",
+  }).start();
 
-    progress.style.width = `${percentComplete}%`;
-  } else {
-    countdownDays.textContent = "We made it!";
-    countdownWeeks.textContent = "🎉 Congratulations!";
-    countdownHours.textContent = "Is he leaving yet?";
-    progress.style.width = `100%`;
+  new CountUp(hoursText, diffHours, {
+    startVal: totalHours,
+    separator: "",
+  }).start();
+
+  new CountUp(progressLabel, percentComplete, {
+    suffix: "% complete",
+    decimalPlaces: 1,
+  }).start();
+
+  progress.style.width = `${percentComplete}%`;
+
+  if (percentComplete >= 100) {
     progress.style.backgroundColor = "green";
-    progressLabel.textContent = `100% complete!`;
   }
 }
 
 const startTime = new Date(2025, 0, 20, 12, 0, 0);
 const endTime = new Date(2029, 0, 20, 12, 0, 0);
+
 setCountdown(startTime, endTime);
